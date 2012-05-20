efc0aec1ab66bf77258fa514cd88fddd9f0b4670
Karel Minarik <karmi@karmi.cz>
2012-05-20 18:15:11 +0200

    [02] Implement periodic polling and updating existing collection
    * Cluster State endpoint is polled periodically
    * New indices are added to collection
    * Removed indices are removed from the collection
    * Collection items are updated based on JSON received from ElasticSearch
    * Ember collection <> DOM binding takes care of updating the HTML

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var App = Em.Application.create({
   name: "Paramedic",
 
   ready: function() {
-    App.indices.initialize();
+    l(App.name + ' loaded.')
+    App.indices.refresh();
+    setInterval(function() { App.indices.refresh() }, 1000);
     return this._super()
   ;}
 });
@@ -25,42 +27,63 @@ App.Index.Shard = Ember.Object.extend({
 App.indices = Ember.ArrayController.create({
   content: [],
 
-  initialize: function() {
+  contains: function(item) {
+    return (Ember.typeOf(item) == 'string') ? this.mapProperty('name').contains(item) : this._super();
+  },
+
+  refresh: function() {
     var self = this;
 
     $.getJSON("http://localhost:9200/_cluster/state", function(data) {
       for (var index_name in data.routing_table.indices) {
 
-        var index = App.Index.create({
-          name: index_name,
-          state: data.metadata.indices[index_name]['state'],
-          settings: Ember.Object.create({
+        // Create or find an index
+        //
+        if ( !self.contains(index_name) ) self.addObject(App.Index.create({ name: index_name }))
+        var index = self.findProperty("name", index_name)
+
+        // Update index properties
+        //
+        index
+          .set("state", data.metadata.indices[index_name]['state'])
+
+          .set("settings", Ember.Object.create({
             number_of_replicas: data.metadata.indices[index_name]['settings']['index.number_of_replicas']
-          }),
-          // mappings: data.metadata.indices[index_name]['mappings'],
-          aliases: data.metadata.indices[index_name]['aliases']
-        });
+          }))
 
-        index.set("shards", function() {
-          var shards = []
-          for (var shard_name in data.routing_table.indices[index_name]['shards']) {
+          .set("aliases", data.metadata.indices[index_name]['aliases'])
 
-            data.routing_table.indices[index_name]['shards'][shard_name].forEach(function(shard) {
-              l(shard)
+          .set("shards", function() {
+            var shards = []
+            for (var shard_name in data.routing_table.indices[index_name]['shards']) {
 
-              shards.addObject(App.Index.Shard.create({
-                name: shard.shard,
-                state: shard.state,
-                primary: shard.primary,
-                node_id: shard.node,
-                relocating_node_id: shard.relocating_node
-              }));
-            });
+              data.routing_table.indices[index_name]['shards'][shard_name].forEach(function(shard) {
+                // l(shard)
+                shards.addObject(App.Index.Shard.create({
+                  name: shard.shard,
+                  state: shard.state,
+                  primary: shard.primary,
+                  node_id: shard.node,
+                  relocating_node_id: shard.relocating_node
+                }));
+              });
+            }
+            return shards.sort(function(a,b) {return a.get('primary') < b.get('primary')})
+          }())
+
+        // Remove deleted indices from the collection
+        // TODO: Use model instance identity for this
+        //
+        self.forEach(function(item) {
+          // console.log(item.name)
+          var loc = self.content.length || 0
+          while(--loc >= 0) {
+            var curObject = self.content.objectAt(loc)
+            if ( !Ember.keys(data.routing_table.indices).contains(item.name) && curObject.name === item.name) {
+              self.content.removeAt(loc)
+            }
           }
-          return shards.sort(function(a,b) {return a.get('primary') < b.get('primary')})
-        }())
-
-        self.addObject(index);
+        })
       }
     });
   }
