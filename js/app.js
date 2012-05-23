60394771eb2d56346362d6599763d289e09e5aa8
Karel Minarik <karmi@karmi.cz>
2012-05-23 20:10:37 +0200

    [15] Added Index#url computed property

--- js/app.js
+++ js/app.js
@@ -23,6 +23,9 @@ App.Node = Ember.Object.extend({
 });
 
 App.Index = Ember.Object.extend({
+  url: function() {
+    return App.elasticsearch_url + '/' + this.name + '/_search?pretty'
+  }.property("name").cacheable()
 });
 
 App.Index.Shard = Ember.Object.extend({
